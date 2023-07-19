ad3bc3980d5a90eeb5ea64751a2952051ada76d8
he1l0world <dawson.chen97@gmail.com>
2023-07-18 22:48:01 -0500

    change the implementation of two sum

--- src/main/java/org/leetcode/solutions/TwoSum.java
+++ src/main/java/org/leetcode/solutions/TwoSum.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap();
-        for(int index = 0; index < nums.length; index++){
-            map.put(nums[index], index);   // put (each num, key) in a map
-            int expectedNum = target - nums[index];
-            int expectedIndex = map.getOrDefault(expectedNum, null);
-            if( Optional.ofNullable(expectedIndex).isPresent() && index != expectedIndex )
-                return new int[]{index, map.get(expectedNum)};
+        for(int currentIndex = 0; currentIndex < nums.length; currentIndex++){
+            Integer previouslyIndex = map.putIfAbsent(nums[currentIndex], currentIndex);   // put (each num, key) in a map
+            if(Optional.ofNullable(previouslyIndex).isPresent() && nums[currentIndex] + nums[previouslyIndex] == target)
+                return new int[]{previouslyIndex, currentIndex};
+            int expectedNum = target - nums[currentIndex];
+            Integer expectedIndex = map.getOrDefault(expectedNum, null);
+            if( Optional.ofNullable(expectedIndex).isPresent() && currentIndex != expectedIndex )
+                return new int[]{currentIndex, map.get(expectedNum)};
         }
 
         return null;
