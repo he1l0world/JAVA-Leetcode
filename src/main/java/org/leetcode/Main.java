3976e67c403e995ebd9702c71e676159129280f8
he1l0world <dawson.chen97@gmail.com>
2023-07-27 00:55:48 -0500

    clean up

--- src/main/java/org/leetcode/Main.java
+++ src/main/java/org/leetcode/Main.java
@@ -1,7 +1,6 @@
 package org.leetcode;
 public class Main {
     public static void main(String[] args) {
-        String string  = "Aabc";
-        System.out.println('a' - 'b');
+        System.out.println("Hello leetcode!");
     }
 }
